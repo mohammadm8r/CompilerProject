68f4f5657309e1e3fe1d2b0db8a5a49e076c2b22
Mohammad <mrezaei76@yahoo.com>
2019-01-18 12:35:27 +0330

    LEXER Completed

--- lexer.py
+++ lexer.py
@@ -8,10 +8,36 @@ tokens = (
     'PARENTHESES_OPEN',
     'PARENTHESES_CLOSE',
     'BRACE_OPEN',
-    'BRACE_CLOSE'
-
+    'BRACE_CLOSE',
+    'SEMICOLON',
+    'DOT',
+    'COMMA',
+    'TILDA',
+    'AND',
+    'OR',
+    'LOGICAL_OR',
+    'LOGICAL_AND',
+    'PLUS',
+    'MINUS',
+    'MULTIPLY',
+    'DIVIDE',
+    'PERCENTAGE',
+    'QUESTION_MARK',
+    'TWO_POINTS',
+    'PLUS_PLUS',
+    'MINUS_MINUS',
+    'EQUAL',
+    'PLUS_EQUAL',
+    'MINUS_EQUAL',
+    'MULTIPLY_EQUAL',
+    'DIVISION_EQUAL',
+    'LEQ',
+    'GEQ',
+    'EEQ',
+    'NEQ',
+    'LT',
+    'GT'
 )
-
 
 t_PARENTHESES_OPEN = r'\('
 t_PARENTHESES_CLOSE = r'\)'
@@ -19,6 +45,35 @@ t_BRACKET_OPEN = r'\['
 t_BRACKET_CLOSE = r'\]'
 t_BRACE_OPEN = r'\{'
 t_BRACE_CLOSE = r'\}'
+t_SEMICOLON = r'\;'
+t_DOT = r'\.'
+t_COMMA = r'\,'
+t_LOGICAL_AND = r'&&'
+t_LOGICAL_OR = r'\|\|'
+t_OR = r'or'
+t_AND = r'and'
+t_TILDA = r'~'
+t_QUESTION_MARK = r'\?'
+t_MINUS = r'\-'
+t_PLUS = r'\+'
+t_MULTIPLY = r'\*'
+t_PERCENTAGE = r'%'
+t_DIVIDE = r'/'
+t_TWO_POINTS = r':'
+t_PLUS_PLUS = r'\+\+'
+t_MINUS_MINUS = r'\-\-'
+t_EQUAL = r'='
+t_PLUS_EQUAL = r'\+='
+t_MINUS_EQUAL = r'\-='
+t_MULTIPLY_EQUAL = r'\*='
+t_DIVISION_EQUAL = r'/='
+t_LEQ = r'<='
+t_GEQ = r'>='
+t_EEQ = r'=='
+t_LT = r'<'
+t_GT = r'>'
+t_NEQ = r'!='
+
 
 
 def t_NUMBER(t):
